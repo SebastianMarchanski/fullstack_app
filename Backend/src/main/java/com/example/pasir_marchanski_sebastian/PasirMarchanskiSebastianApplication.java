25dc1ce475897911182e2d871b76381fcd247082
Sebastian Marchański <sebastian.marchanski@student.pk.edu.pl>
2025-06-09 19:44:07 +0200

    initial monorepo setup: frontend + backend

---
+++ Backend/src/main/java/com/example/pasir_marchanski_sebastian/PasirMarchanskiSebastianApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasir_marchanski_sebastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasirMarchanskiSebastianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasirMarchanskiSebastianApplication.class, args);
+    }
+
+}
---
+++ Backend/src/main/java/com/example/pasir_marchanski_sebastian/model/TransactionType.java
@@ -0,0 +1,6 @@
+package com.example.pasir_marchanski_sebastian.model;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE
+}
